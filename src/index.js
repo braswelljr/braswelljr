d9e00e12efe1fa843067be4fe2efbc3981c4b326
braswelljr <braswellkenneth7@gmail.com>
2020-12-24 23:36:15 +0000

    configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,38 +7,10 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      xs: "320px",
-      sm: "480px",
-      md: "640px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px"
-    },
-    extend: {
-      fontFamily: {
-        langar: ["'Langar'", "display"],
-        mulish: ["'Mulish'", "system-ui", "sans-serif"],
-        monoton: ["'Monoton'", "display"]
-      },
-      colors: {},
-      animation: {
-        moveBackground: "moveBackground 10s ease-in alternate infinite"
-      },
-      keyframes: {
-        moveBackground: {
-          "0%": { backgroundPosition: "0 50%" },
-          "50%": { backgroundPosition: "50% 100%" },
-          "100%": { backgroundPosition: "50% 0" }
-        }
-      }
-    }
+    extend: {}
   },
   variants: {
-    extend: {
-      display: ["focus-within", "hover", "active", "focus"],
-      ringOffsetWidth: ["hover", "active"]
-    }
+    extend: {}
   },
-  plugins: [require("@tailwindcss/aspect-ratio")]
+  plugins: []
 };
