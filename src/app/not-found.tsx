e0f3569e3034cab141a74fd882b7b1f24019a92a
Braswell Kenneth Azu Junior <braswellkenneth7@gmail.com>
2025-10-27 02:42:44 +0000

    fix: not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function Custom404() {
   return (
-    <section className={'fixed inset-0 grid h-screen place-content-center px-4 text-center'}>
+    <section className="grid h-screen place-content-center px-4 text-center">
       <div className="">
         <div className="flex flex-auto flex-col items-center justify-center sm:flex-row">
           <h1 className="text-7xl font-extrabold tracking-tight text-neutral-900 sm:mr-6 sm:border-r sm:border-neutral-900/50 sm:pr-6 dark:text-white/40 sm:dark:border-white/50">
