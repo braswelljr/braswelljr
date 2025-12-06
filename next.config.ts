bd2cd080a1c9e18832c32307b7a0da425e0c65d6
Braswell Kenneth Azu Junior <braswellkenneth7@gmail.com>
2025-12-06 15:59:56 +0000

    chore: upgrade deps

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import { createMDX } from 'fumadocs-mdx/next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['oxc-transform', 'typescript', 'twoslash']
+  serverExternalPackages: ['oxc-transform', 'typescript', 'twoslash', 'shiki']
 };
 
 const withMDX = createMDX({});
